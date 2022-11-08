245d452505fd7eb1070325b2767f469312ab9774
Alain Vega <alainjosevz@gmail.com>
2022-11-08 12:28:29 -0300

    UI v1.1
    Nuevo:
    *script generalizado para cambiar de escena (ScriptChangeScene)
    *elegir la ficha de j1 y j2 en configuración de la partida (en ScriotGameConfiguration)
    Eliminado:
    *scripts innecesarios de control para cada escena, excepto para la de configuración de una partida.

--- Assets/Scripts/ScriptGameConfiguration.cs
+++ Assets/Scripts/ScriptGameConfiguration.cs
@@ -1,19 +1,22 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScriptGameConfiguration : MonoBehaviour
 {
-    public Button buttonMainMenu, buttonConfiguration, buttonCredits;
+    public Button buttonChipPlayer1, buttonChipPlayer2;
+    public Image imageChipPlayer1, imageChipPlayer2;
+    public Sprite[] chipSprites;
 
     // Start is called before the first frame update
     void Start()
     {
-        buttonMainMenu.onClick.AddListener(GoSceneMainMenu);
-        buttonConfiguration.onClick.AddListener(GoSceneConfiguration);
-        buttonCredits.onClick.AddListener(GoSceneCredits);
+        buttonChipPlayer1.onClick.AddListener(delegate{ ChangeImageChipPlayer(1); });
+        buttonChipPlayer2.onClick.AddListener(delegate{ ChangeImageChipPlayer(2); });
     }
 
     // Update is called once per frame
@@ -21,17 +24,23 @@ public class ScriptGameConfiguration : MonoBehaviour
     {
         
     }
-
-    public void GoSceneMainMenu()
+    public void ChangeImageChipPlayer(int player)
     {
-        SceneManager.LoadScene("SceneMainMenu");
-    }
-    public void GoSceneConfiguration()
-    {
-        SceneManager.LoadScene("SceneConfiguration");
-    }
-    public void GoSceneCredits()
-    {
-        SceneManager.LoadScene("SceneCredits");
+        Image imageChip = player == 1 ? imageChipPlayer1 : imageChipPlayer2;
+        for (int i = 0; i < chipSprites.Length; i++)
+        {
+            if (imageChip.sprite == chipSprites[i])
+            {
+                if (i < chipSprites.Length - 1)
+                {
+                    imageChip.sprite = chipSprites[i + 1];
+                }
+                else
+                {
+                    imageChip.sprite = chipSprites[0];
+                }
+                break;
+            }
+        }
     }
 }
