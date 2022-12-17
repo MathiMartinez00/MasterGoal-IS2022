38291f253318ef87529de90eba6eb0ea02d02dca
Elioth <56732944+elioth-frutos@users.noreply.github.com>
2022-12-17 10:21:05 -0300

    Deleted some unnecessary fields on the ScriptController class.

--- Assets/Scripts/ScriptController.cs
+++ Assets/Scripts/ScriptController.cs
@@ -25,20 +25,11 @@ public class ScriptController : MonoBehaviour
     public GameObject whitePiece2;
     public GameObject blackPiece1;
     public GameObject blackPiece2;
-    public GameObject ballPiece;
+    public GameObject ball;
     
     // Movement settings
-    public float smoothDampTime = .02f;
-    public float distanceToSnap = .05f;
-
-    // Variables for game control used in movement validation
-    [SerializeField] private int passCount = 0;
-    List<Vector3> destinationsPlayer = new List<Vector3>();
-    List<Vector3> destinationsBall = new List<Vector3>();
-    [SerializeField] private GameObject selectedChip;
-    [SerializeField] private GameObject ballChipPasser;
-    public Team currentTurn;
-    public PlayerStates currentState;
+    //public float smoothDampTime = .02f;
+    //public float distanceToSnap = .05f;
 
     // UI variables
     public TextMeshProUGUI whiteScoreText, redScoreText;
@@ -50,78 +41,14 @@ public class ScriptController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        whiteName = PlayerPrefs.GetString("player1");
-        redName = PlayerPrefs.GetString("player2");
-        whiteScoreName.text = whiteName;
-        redScoreName.text = redName;
-        currentTurn = Team.Red;
-        ballChip.GetComponent<ScriptBall>().teamPossession = currentTurn;
-        currentState = PlayerStates.WaitingPlayerInputChip;
-        boardBoxCollider = tilemapBoard.gameObject.GetComponent<BoxCollider2D>();
-        ResetState();
+        this.whiteName = PlayerPrefs.GetString("player1");
+        this.redName = PlayerPrefs.GetString("player2");
+        this.whiteScoreName.text = whiteName;
+        this.redScoreName.text = redName;
+        this.boardBoxCollider =
+        tilemapBoard.gameObject.GetComponent<BoxCollider2D>();
         // Create a new abstract game instance.
-        game = New Game(2Players, White);
-    }
-
-    IEnumerator MoveChipAndUpdateState(GameObject chip, Vector3 destination)
-    {
-        // Moves the chip to the destination passed with SmoothDamp (destination MUST be in world units) and then
-        // updates the game state.
-        while (chip.transform.position != destination)
-        {
-            Vector2 velocity = Vector2.zero;
-            chip.transform.position = Vector2.SmoothDamp(
-                chip.transform.position,
-                destination,
-                ref velocity,
-                smoothDampTime);
-            if (
-                Math.Abs(Vector2.Distance(
-                    chip.transform.position, destination)) < distanceToSnap
-                    )
-            {
-                chip.transform.position = destination;
-            }
-            yield return null;
-        }
-        if (IsBallInGoal())
-        {
-            // Update score UI and reset variables.
-            if (currentTurn == Team.White)
-            {
-                whiteScore++;
-                whiteScoreText.text = whiteScore.ToString();
-            }
-            else
-            {
-                redScore++;
-                redScoreText.text = redScore.ToString();
-            }
-            if (redScore >= 2 || whiteScore >= 2)
-            {
-                string winner = currentTurn == Team.White ? whiteName : redName;
-                winnerName.text = "Gano " + winner;
-                boardBoxCollider.enabled = false;
-            }
-            ResetState();
-            yield return null;
-        }
-        else
-        {
-            // When move is done, check if more moves are available (passes)
-            if (IsBallPassable(chip))
-            {
-                passCount++;
-                currentState = PlayerStates.WaitingPlayerInputBallDestination;
-                yield return StartCoroutine(CalculateMovesBallChip(destinationsBall));
-            }
-            else
-            {
-                passCount = 0;
-                currentTurn = currentTurn == Team.White ? Team.Red : Team.White;
-                ballChip.GetComponent<ScriptBall>().teamPossession = currentTurn;
-          }
-        }
+        this.game = New Game(2Players, White);
     }
 
     // This method is called whenever the user taps on the screen.
