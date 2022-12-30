86e5a01d17b4c7e2fd1b7cf3682fcd01ad637d9c
Elioth <56732944+elioth-frutos@users.noreply.github.com>
2022-12-30 08:04:28 -0300

    Added sound to the game. Created an AudioManager game object and prefab.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public Sound[] music;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+        foreach (Sound s in music)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        PlayMusic("Hypnotic puzzle");
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            Debug.LogWarning("Sound: " + name + " not found.");
+
+        s.source.Play();
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(music, s => s.name == name);
+
+        if (s == null)
+            Debug.LogWarning("Song: " + name + " not found.");
+
+        s.source.Play();
+    }
+
+}
